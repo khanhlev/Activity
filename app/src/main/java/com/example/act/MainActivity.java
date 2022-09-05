1dc89bb26cba2a3459b12d53bfbc91f85f532f78
VanKhanh <khanhle0767251658@gmail.com>
2022-09-05 10:45:07 +0700

    first commit

---
+++ app/src/main/java/com/example/act/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.act;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+}}
